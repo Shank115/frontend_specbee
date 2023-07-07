326c0613e24d25006ef01d4de2eba90c3c031632
Shank115 <i.shashnks@gmail.com>
2023-07-07 22:40:21 +0530

    dsfdc

---
+++ js/ambition.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider').slick( {
+        infinite: true,
+        slidesToShow: 4,
+        autoplay: true,
+        autoplaySpeed: 10,
+    });
+});
--- js/hamburg.js
+++ js/hamburg.js
@@ -1,5 +1,20 @@
-function hamburger(element) {
-    var hamburgMenu = document.getElementById("hamburgMenu");
-    hamburgMenu.classList.toggle("menu");
-    element.classList.toggle("lines");
-}
+document.addEventListener('DOMContentLoaded', function () {
+    // Select the toggle button and menu element
+    var toggleButton = document.querySelector('.header__nav-toggle');
+    var navMenu = document.getElementById('navMenu');
+
+    // Add a click event listener to the toggle button
+    toggleButton.addEventListener('click', function () {
+        var body = document.body;
+
+        if (navMenu.classList.contains('active')) {
+            navMenu.classList.remove('active');
+            body.classList.remove('no-scroll');
+            toggleButton.classList.remove('open');
+        } else {
+            navMenu.classList.add('active');
+            body.classList.add('no-scroll');
+            toggleButton.classList.add('open');
+        }
+    });
+});
